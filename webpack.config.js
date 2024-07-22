97b1081cf5f17d5c24106210bed74df65cd82a7a
Bryan Ho <9xbryan@gmail.com>
2024-07-22 12:59:08 -0700

    uisng latest npm packages"

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-var path = require('path');
-var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var path = require( 'path' );
+var MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 //var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry:path.resolve(__dirname) + '/src/index.js',
+  entry: path.resolve( __dirname ) + '/src/index.js',
   externals: {
     react: 'commonjs react',
     'react-dom': 'commonjs react-dom'
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve( __dirname, 'build' ),
     filename: 'index.js',
     libraryTarget: 'commonjs2'
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [ new MiniCssExtractPlugin() ],
   resolve: {
     alias: {
-      react: path.resolve('./node_modules/react')
+      react: path.resolve( './node_modules/react' )
     }
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
+        include: path.resolve( __dirname, 'src' ),
         exclude: /(node_modules|bower_components|build)/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: [ '@babel/preset-env' ]
           }
         }
       }, {
         test: /\.*css$/i,
-        use : [MiniCssExtractPlugin.loader, 'css-loader'] 
+        use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
       },
     ]
   }
